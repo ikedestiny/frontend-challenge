4a49da4e6159b84ee162a2edd65cf5500c31a712
Ike Holy Destiny <121898018+ikedestiny@users.noreply.github.com>
2025-01-19 16:13:53 +0300

    deploy

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,15 @@
+import { useNavigate } from 'react-router-dom';
 import '../style/Navbar.css'
 export default function Navbar() {
+    const navigate = useNavigate()
+    const handleClick = (e, link) => {
+        e.preventDefault();
+        navigate(link);
+    };
     return (
         <div className="navbar">
-            <a href="/frontend-challenge/">все котики</a>
-            <a href="/frontend-challenge/favorites">Любимые котики</a>
+            <button className="nav-link active" onClick={(e) => handleClick(e, "/frontend-challenge/")}>все котики</button>
+            <button className="nav-link active" onClick={(e) => handleClick(e, "/frontend-challenge/favorites")}>Любимые котики</button>
         </div>
     )
 }
