007d3eeb8b574700d14c2f03df96f97d0ee6c580
Ike Holy Destiny <121898018+ikedestiny@users.noreply.github.com>
2025-01-19 16:22:29 +0300

    deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-
-import AllCats from './components/AllCats';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/NavBar';
+import AllCats from './components/AllCats';
 import FavoriteCats from './components/FavoriteCats';
 
 function App() {
@@ -11,12 +9,12 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route exact path='/frontend-challenge/' element={<AllCats />} />
-          <Route exact path='/frontend-challenge/favorites' element={<FavoriteCats />} />
+          <Route exact path='/' element={<AllCats />} />
+          <Route exact path='/favorites' element={<FavoriteCats />} />
         </Routes>
       </Router>
     </div>
   );
 }
 
-export default App
+export default App;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,27 @@
 import { useNavigate } from 'react-router-dom';
-import '../style/Navbar.css'
+import '../style/Navbar.css';
+
 export default function Navbar() {
-    const navigate = useNavigate()
-    const handleClick = (e, link) => {
-        e.preventDefault();
-        navigate(link);
+    const navigate = useNavigate();
+
+    const handleClick = (path) => {
+        navigate(path);
     };
+
     return (
         <div className="navbar">
-            <button className="nav-link active" onClick={(e) => handleClick(e, "/frontend-challenge/")}>все котики</button>
-            <button className="nav-link active" onClick={(e) => handleClick(e, "/frontend-challenge/favorites")}>Любимые котики</button>
+            <button
+                className="nav-link"
+                onClick={() => handleClick("/")}
+            >
+                Все котики
+            </button>
+            <button
+                className="nav-link"
+                onClick={() => handleClick("/favorites")}
+            >
+                Любимые котики
+            </button>
         </div>
-    )
+    );
 }
